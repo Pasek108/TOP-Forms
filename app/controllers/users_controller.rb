9101f75ef6e7a6f188638d35c132a31295068a90
Pasek108 <pasartur108@gmail.com>
2025-08-29 10:51:16 +0200

    edit form route and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,28 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save()
+    if @user.save
+      flash[:success] = "User was successfully updated"
       redirect_to new_user_path
     else
+      flash.now[:error] = "Something went wrong"
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:success] = "User was successfully updated"
+      redirect_to @user
+    else
+      flash.now[:error] = "Something went wrong"
+      render "edit", :unprocessable_entity
     end
   end
 
